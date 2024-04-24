d0be41a16b8d3d11bcdccf2b464cb856ced73fa6
Kirill67tyar <kirillbogomolov.ric@gmail.com>
2024-04-24 17:09:57 +0300

    Start work with Order.

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,9 +1,52 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
+from recipes.models import (
+    Recipe,
+    Ingredient,
+)
 
-# class Order(models.Model):
-#     owner = ...
-#     pass
+
+User = get_user_model()
 
 
-# Create your models here.
+class Order(models.Model):
+    owner = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='orders',
+        verbose_name='Заказчик',
+    )
+    downloaded = models.BooleanField(
+        default=False,
+        verbose_name='Скачан',
+    )
+
+class RecipeOrder(models.Model):
+    order = models.ForeignKey(
+        to=Order,
+        on_delete=models.CASCADE,
+        related_name='items',
+        verbose_name='Заказ'
+    )
+    recipe = models.ForeignKey(
+        to=Recipe,
+        on_delete=models.CASCADE,
+        related_name='orders',
+        verbose_name='Рецепт'
+    )
+    # quantity = models.PositiveSmallIntegerField( # ! если рецепты могут дублироваться
+    #     default=1,
+    #     verbose_name='Количество'
+    # )
+
+"""
+3 этапа:
+
+* 1 - написать эндпоинты для добавления/удаления рецептов в заказе, и чтобы заказ нормально создавался
+* 2 - написать алгоритм для складывания и подсчёта ингредиентов, хорошо подойдёт словарь {ингредиент: кол-во}
+* 3 - написать обрабочтк, для того чтобы отдавать пдф документ клиенту. При этом должно фиксироваться что заказ скачан,
+*     и должен формироваться новый заказ после того, как старый скачан
+
+"""
+
